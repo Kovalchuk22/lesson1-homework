becd840f02d49f70fa11b308bfb4b5687aafcb59
Kovalchuk22 <153983720+Kovalchuk22@users.noreply.github.com>
2024-12-13 15:10:30 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,37 @@
+// Завдання 1
+const age = 14;
+console.log(age);
+
+// Завдання 2
+const name = 'Taras';
+console.log(name);
+
+// Завдання 3
+const isStudent = false;
+console.log(isStudent);
+
+// Завдання 4
+const myString = "“Найскладніше почати діяти, все інше залежить тільки від наполегливості.”, – Амелія Ергарт.";
+console.log(myString);
+
+// Завдання 5
+let myNumber = 22;
+myNumber = myNumber + 10;
+console.log(myNumber);
+
+// Завдання 6
+const myNull = null;
+console.log(myNull);
+
+// Завдання 7
+let userName = prompt('Як вас звати?');
+alert('Вітаю, ' + userName + '!');
+
+// Завдання 8
+const ask = confirm("Ви підтверджуєте дію?");
+alert(ask ? "Дякую за підтвердження!" : "Дію відмінено!")
+
+// Завдання 9
+alert("Увага! Ви збираєтесь виконати небезпечну дію.");
+const ask2 = confirm("Ви впевнені, що хочете продовжити?");
+alert(ask2 ? "Дякую за підтвердження!" : "Дію відмінено!");
